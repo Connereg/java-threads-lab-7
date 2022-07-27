17d68f8612b0d4770e07e43e1876b370b5c6a3d7
Conner Gleason <connereg@optonline.net>
2022-07-27 13:42:53 -0400

    Completed Lab (Cleaned Code)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,33 +19,3 @@ public class Main {
     }
 }
 
-//import java.util.concurrent.*;
-//
-//class Example {
-//    public static void main(String[] args) throws Exception {
-//        ExecutorService executor = Executors.newFixedThreadPool(5);
-//
-//        Future<Integer> num1 = executor.submit(()-> {
-//            try {
-//                Thread.sleep(3000);
-//                return 1000;
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        });
-//
-//        Future<Integer> num2 = executor.submit(()-> {
-//            try {
-//                Thread.sleep(500);
-//                return 2000;
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        });
-//
-//        int result = num1.get() * num2.get();
-//        System.out.println(result); // 2000000
-//
-//        executor.shutdown();
-//    }
-//}
